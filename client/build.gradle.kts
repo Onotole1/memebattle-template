9a45d9bc74c94b9680fb0a586479dc95506c2be8
seva <szhukov@icerockdev.com>
2020-02-26 00:56:36 +0700

    add naviagetion deps

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -32,5 +32,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.navigation:navigation-fragment:2.2.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.2.1")
     implementation(project(":common"))
 }
