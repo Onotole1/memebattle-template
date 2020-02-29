6f91ce7a11669ac0438e0c806774e2daa496b684
Анатолий Спитченко <a.spitchenko@cft.ru>
2020-02-29 23:08:38 +0700

    Возвращаю рандомный рейтинг

--- common/src/main/kotlin/ru/memebattle/common/dto/user/UserResponseDto.kt
+++ common/src/main/kotlin/ru/memebattle/common/dto/user/UserResponseDto.kt
@@ -3,6 +3,7 @@ package ru.memebattle.common.dto.user
 data class UserResponseDto(
     val id: Long,
     val username: String,
+    val rating: Int,
     val average: Float,
     val semesters: List<SemesterDto>
 )
--- server/src/main/kotlin/ru/memebattle/model/UserModel.kt
+++ server/src/main/kotlin/ru/memebattle/model/UserModel.kt
@@ -2,6 +2,7 @@ package ru.memebattle.model
 
 import io.ktor.auth.Principal
 import ru.memebattle.common.dto.user.UserResponseDto
+import kotlin.random.Random
 
 data class UserModel(
     val id: Long = 0,
@@ -9,4 +10,10 @@ data class UserModel(
     val password: String
 ): Principal
 
-fun UserModel.toDto() = UserResponseDto(id, username, 0F, emptyList())
+fun UserModel.toDto() = UserResponseDto(
+    id,
+    username,
+    Random.nextInt(10),
+    Random.nextFloat(),
+    emptyList()
+)
