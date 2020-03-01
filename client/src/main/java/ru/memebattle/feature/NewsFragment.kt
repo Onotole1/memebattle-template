ea6a901cacb16cc727c9d212bc3459892e53287a
LukianBat <lukianzhukov2000@gmail.com>
2020-03-01 10:14:55 +0300

    add news

--- client/src/main/java/ru/memebattle/feature/NewsFragment.kt
+++ client/src/main/java/ru/memebattle/feature/NewsFragment.kt
@@ -6,17 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.addTo
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_news.*
 import org.koin.android.ext.android.get
 import ru.memebattle.ARGS_POST
 import ru.memebattle.R
 import ru.memebattle.common.dto.PostResponseDto
 import ru.memebattle.core.BaseFragment
 import ru.memebattle.core.api.NewsApi
-import ru.memebattle.core.utils.toast
 
 class NewsFragment : BaseFragment() {
 
@@ -39,21 +34,5 @@ class NewsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        progressBar.visibility = View.VISIBLE
-
-        newsApi.getPosts()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                recyclerViewNews?.adapter = NewsAdapter(it, onItemClickListener)
-
-                progressBar?.visibility = View.GONE
-            }, {
-                toast(it.localizedMessage)
-
-                progressBar.visibility = View.GONE
-            })
-            .addTo(compositeDisposable)
     }
 }
