fd2b1e526af65649b734810ef3bcdb9d4ea7539a
Onotole1 <jnsndp161>
2020-05-29 21:18:28 +0700

    multipart fixed

--- server/src/main/kotlin/ru/memebattle/service/FileService.kt
+++ server/src/main/kotlin/ru/memebattle/service/FileService.kt
@@ -20,52 +20,56 @@ import java.util.*
 
 class FileService(private val uploadPath: String) {
 
-    init {
-        if (Files.notExists(Paths.get(uploadPath))) {
-            Files.createDirectory(Paths.get(uploadPath))
-        }
-    }
+	init {
+		if (Files.notExists(Paths.get(uploadPath))) {
+			Files.createDirectory(Paths.get(uploadPath))
+		}
+	}
 
-    suspend fun save(multipart: MultiPartData): MediaResponseDto {
-        var response: MediaResponseDto? = null
-        multipart.forEachPart { part ->
-            when (part) {
-                is PartData.FileItem -> {
-                    if (part.name == "file") {
-                        BufferedInputStream(part.streamProvider()).use {
-                            val parser = AutoDetectParser()
-                            val detector = parser.detector
-                            val md = Metadata()
-                            val mediaType = withContext(Dispatchers.IO) {
-                                detector.detect(it, md)
-                            }
-                            if (mediaType.type != "image") {
-                                throw UnsupportedMediaTypeException(part.contentType ?: ContentType.Any)
-                            }
-                        }
-                        val ext = when (part.contentType) {
-                            ContentType.Image.JPEG -> "jpg"
-                            ContentType.Image.PNG -> "png"
-                            else -> throw UnsupportedMediaTypeException(part.contentType!!)
-                        }
-                        val name = "${UUID.randomUUID()}.$ext"
-                        val path = Paths.get(uploadPath, name)
-                        part.streamProvider().use {
-                            withContext(Dispatchers.IO) {
-                                Files.copy(it, path)
-                            }
-                        }
-                        part.dispose()
-                        response = MediaResponseDto(name, MediaType.IMAGE)
-                        return@forEachPart
-                    }
-                }
-            }
+	suspend fun save(multipart: MultiPartData): MediaResponseDto {
+		var response: MediaResponseDto? = null
+		multipart.forEachPart { part ->
+			when (part) {
+				is PartData.FileItem -> {
+					if (part.name == "file") {
+						BufferedInputStream(part.streamProvider()).use { buffer ->
+							val parser = AutoDetectParser()
+							val detector = parser.detector
+							val md = Metadata()
+							val mediaType = withContext(Dispatchers.IO) {
+								detector.detect(buffer, md)
+							}
+							if (mediaType.type != "image") {
+								throw UnsupportedMediaTypeException(
+									part.contentType ?: ContentType.Any
+								)
+							}
 
-            part.dispose()
-        }
-        @Suppress("EXPERIMENTAL_API_USAGE")
-        return response ?: throw BadRequestException("No file field in request")
-    }
+							val ext = when (part.contentType) {
+								ContentType.Image.JPEG -> "jpg"
+								ContentType.Image.PNG  -> "png"
+								else                   -> throw UnsupportedMediaTypeException(part.contentType!!)
+							}
+							val name = "${UUID.randomUUID()}.$ext"
+							val path = Paths.get(uploadPath, name)
+
+							withContext(Dispatchers.IO) {
+								Files.copy(buffer, path)
+							}
+
+							part.dispose()
+							response = MediaResponseDto(name, MediaType.IMAGE)
+							return@forEachPart
+						}
+					}
+
+					part.dispose()
+				}
+			}
+		}
+
+		@Suppress("EXPERIMENTAL_API_USAGE")
+		return response ?: throw BadRequestException("No file field in request")
+	}
 }
 
